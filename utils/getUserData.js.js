67780e20230a69349184aecd1f5139ecb3041dfd
Olatunde Amos <98148042+akoredetunde@users.noreply.github.com>
2022-09-21 17:04:16 +0100

    created a function to return user data

---
+++ utils/getUserData.js.js
@@ -0,0 +1,22 @@
+async function getUserData(){
+  // url this is just a dommy url from the dommy server i created
+  let url = "http://127.0.0.1:3000/projects/amos";
+  let option = {
+    method: "GET",
+    headers:{
+      Accept: "application/json"
+    }
+  }
+  try{
+    let response = await fetch(url, option);
+    let data = await response.json();
+     return data;
+  }catch(err){
+    return err;
+  }
+}
+
+// calling the function
+getUserData().then(result => {
+  console.log(result);
+})
