602393f3d48e2cec55f39d2861b2a16cf3d581da
jriffs <mayjriffs97@gmail.com>
2023-01-25 01:50:26 +0100

    still erroing

--- utils/getBearerToken.js
+++ utils/getBearerToken.js
@@ -1,6 +1,6 @@
 export default function getBearer(NetworkRequest) {
     const authType = NetworkRequest?.headers?.authorization?.split(' ')[0]
-    console.log(NetworkRequest?.headers?.authorization)
+    console.log(NetworkRequest?.headers)
     if (authType === 'Bearer') {
       console.log(`at getBearer - ${NetworkRequest?.headers?.authorization.split(' ')[1]}`)
       return NetworkRequest?.headers?.authorization.split(' ')[1]
