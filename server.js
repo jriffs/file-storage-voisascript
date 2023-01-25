1de69aa7c3f113e43dd2b013cfb8844953186550
jriffs <mayjriffs97@gmail.com>
2023-01-25 02:08:20 +0100

    more errors

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(preparedFileMiddleware)
 // app.use(localBodyParser) // use when req.body is empty
-/* app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, originator, authorization")
   next()
-}) */
+})
 
 const port = process.env.PORT || 5000;
 
